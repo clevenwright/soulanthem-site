570b115ae8dc9d14245f7d0b49dd0c8d9e4eb691
cleven-EK12 <cwright@empoweringk12.com>
2026-07-07 15:24:51 -0500

    Add animated aurora hero backdrop

---
+++ components/Aurora.tsx
@@ -0,0 +1,11 @@
+export default function Aurora() {
+  return (
+    <div className="aurora" aria-hidden="true">
+      <div className="blob b1" />
+      <div className="blob b2" />
+      <div className="blob b3" />
+      <div className="blob b4" />
+      <div className="blob b5" />
+    </div>
+  );
+}
